621f8c1d8e1f5c28fb09aa94773d57629e858bcd
ryota ohashi <49921015+ryota-ohashi@users.noreply.github.com>
2019-05-04 01:26:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-var $f = jQuery.noConflict();
-
-$f(function(){
-    $f(document).ready(function(){
-		$f('.frame').addClass('addFrame');
-		$f('.name').fadeIn(2000);
+$(function(){
+    $(document).ready(function(){
+		$('.name').fadeIn(2000);
+		$('.menu').fadeIn(2000);
 		
     });
 
-    $f('a[href^=#]').click(function() {
+    $('a[href^=#]').click(function() {
 		var speed = 400;
-		var href= $f(this).attr("href");
-		var target = $f(href == "#" || href == "" ? 'html' : href);
-		var headerHeight = 60; //固定ヘッダーの高さ
-		var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
-		$f('body,html').animate({scrollTop:position}, speed, 'swing');
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var headerHeight = 20; //調整用
+		var position = target.offset().top - headerHeight; 
+		$('body,html').animate({scrollTop:position}, speed, 'swing');
 		return false;
 	});
 	
