6b3579086120a48bbb09d715e7a2dce5cda8ad7c
ryota ohashi <49921015+ryota-ohashi@users.noreply.github.com>
2019-05-12 17:57:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 $(function(){
     $(document).ready(function(){
-		$('.name').fadeIn(2000);
-		$('.menu').fadeIn(2000);
-		
+		$('.logo').fadeIn(2000);
     });
-
-    $('a[href^=#]').click(function() {
-		var speed = 400;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var headerHeight = 20; //調整用
-		var position = target.offset().top - headerHeight; 
-		$('body,html').animate({scrollTop:position}, speed, 'swing');
-		return false;
-	});
-	
 });
