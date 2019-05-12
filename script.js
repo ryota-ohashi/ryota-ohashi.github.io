d527e09c4c6bc4f2c5c9bb78118cd1536ee97301
ryota ohashi <49921015+ryota-ohashi@users.noreply.github.com>
2019-05-12 18:07:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(function(){
     $(document).ready(function(){
 		$('.logo').fadeIn(2000);
     });
+	
+    $(".btn-down").click(function () {
+		var p = $(".contents-text").eq(0).offset().top - 20;
+		$('html,body').animate({ scrollTop: p }, 400);
+		return false;
+    });
 });
