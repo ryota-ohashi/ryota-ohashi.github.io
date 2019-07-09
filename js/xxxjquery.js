ba74d892278cf8bf44edb451f05bc0a7addfe902
ryota ohashi <49921015+ryota-ohashi@users.noreply.github.com>
2019-07-09 15:15:45 +0900

    add comment

--- js/xxxjquery.js
+++ js/xxxjquery.js
@@ -1,5 +1,5 @@
 $(function(){
-
+//     スクロール量によりフェード
     $(window).scroll(function (){
         $('.information').each(function(){
             var isPos = $(this).offset().top,
@@ -12,7 +12,7 @@ $(function(){
             }
         });
     });
-
+//     ナビバー用
     $('#sp-gnav').click(function(){
         $('.header-inner').toggleClass('open');
     });
