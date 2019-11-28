d2870ee109fdfc9f082d888bb878fd402d4f7040
ryota ohashi <49921015+ryota-ohashi@users.noreply.github.com>
2019-11-29 03:04:36 +0900

    modified

---
+++ src/js/_script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function(){
+  
+  // opacityでフェードイン
+  document.getElementById('portfolio').style.opacity = '1';
+
+  // スムーズスクロール
+  $('a[href^="#"]').click(function(){
+    var speed = 400;
+    var href = $(this).attr("href");
+    var tar = $(href == "#" || href == "" ? 'html' : href);
+    var pos = tar.offset().top;
+
+    $('html, body').animate({scrollTop: pos}, speed, 'swing');
+    return false;
+  });
+});
