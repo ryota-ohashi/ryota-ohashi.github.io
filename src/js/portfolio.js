4475f71c6756a47dea48f21af3d682fc00453275
ryota ohashi <ryota.812.skt@gmail.com>
2021-03-21 23:32:51 +0900

    origin

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,85 +1,84 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    // スムーズスクロール
-    $('a[href^="#"]').click(function(){
-    var speed = 400;
-    var href = $(this).attr("href");
-    var tar = $(href == "#" || href == "" ? 'html' : href);
-    var pos = tar.offset().top - 100;
+	// スムーズスクロール
+	$('a[href^="#"]').click(function(){
+	var speed = 400;
+	var href = $(this).attr("href");
+	var tar = $(href == "#" || href == "" ? 'html' : href);
+	var pos = tar.offset().top - 100;
 
-    $('html, body').animate({scrollTop: pos}, speed, 'swing');
-    return false;
-    });
+	$('html, body').animate({scrollTop: pos}, speed, 'swing');
+	return false;
+	});
 
-    //spmenu用
-    $('.header__spmenu').click(function(){
-        $('body').toggleClass('open');
-    });
+	//spmenu用
+	$('.header__spmenu').click(function(){
+		$('body').toggleClass('open');
+	});
 
-    //aタグをクリックしたときにspmenuを閉じる
-    $('.header__gnav__ul__item a').click(function(){
-        if ( $('body').hasClass('open') ) {
-            $('body').removeClass('open');
-        }
-    })
+	//aタグをクリックしたときにspmenuを閉じる
+	$('.header__gnav__ul__item a').click(function(){
+		if ( $('body').hasClass('open') ) {
+			$('body').removeClass('open');
+		}
+	})
 
-    //スクロール関連
-    $(window).on('scroll load', function(){
+	//スクロール関連
+	$(window).on('scroll load', function(){
 
-        let currentPosition = $(window).scrollTop();
-    
-        //ヘッダー用
-        if(currentPosition > 50) {
+		let currentPosition = $(window).scrollTop();
 
-            //spmenuが開いていない時
-            if ( !$('body').hasClass('open') ) {
-                
-                $('.header').css('border', '1px solid #333');
-                $('.header__intro__subtitle').css('display', 'none');
-                $('.header__spmenu').css('top', '9px');
-                //border1px文を加味して上へ(特にsp用)
-                $('.header').css('top', '-1px');
-                //header__bg用
-                $('.header__bg').css('height', '100%');
-            };
-            
-        }
-        else {
+		//ヘッダー用
+		if(currentPosition > 50) {
 
-            if ( $(window).width() > 768 ) {
-                $('.header').css('top', '50px');
-            }
+			//spmenuが開いていない時
+			if ( !$('body').hasClass('open') ) {
 
-            //spmenuが開いていないとき
-            if ( !$('body').hasClass('open') ) {
+				$('.header').css('border', '1px solid #333');
+				$('.header__intro__subtitle').css('display', 'none');
+				$('.header__spmenu').css('top', '9px');
+				//border1px文を加味して上へ(特にsp用)
+				$('.header').css('top', '-1px');
+				//header__bg用
+				$('.header__bg').css('height', '100%');
+			};
+		}
+		else {
 
-                //tab以上の時、headerが動く
-                $('.header').css('border', '1px solid grey');
-                $('.header__intro__subtitle').css('display', 'block');
-                $('.header__spmenu').css('top', '22px');
+			if ( $(window).width() > 768 ) {
+					$('.header').css('top', '50px');
+			}
 
-                //header__bg用
-                $('.header__bg').css('height', '0%');
-            };
-        };
+			//spmenuが開いていないとき
+			if ( !$('body').hasClass('open') ) {
 
-        //背景色変更用
-        let aboutSection = $('#about').offset().top;
-        let skillsSection = $('#skills').offset().top;
-        let portfolioSection = $('#portfolio').offset().top;
-        let contactSection = $('#contact').offset().top;
+				//tab以上の時、headerが動く
+				$('.header').css('border', '1px solid grey');
+				$('.header__intro__subtitle').css('display', 'block');
+				$('.header__spmenu').css('top', '22px');
 
-        let screenHeight = $(window).height();
-        let adjustmentValue = screenHeight / 2;　//調整値
+				//header__bg用
+				$('.header__bg').css('height', '0%');
+			};
+		};
 
-        if( skillsSection - adjustmentValue >= currentPosition && currentPosition >= aboutSection - adjustmentValue ) {
-            $('.bg').css('background-color', '#ccc');
-        } else if( portfolioSection - adjustmentValue >= currentPosition && currentPosition >= skillsSection - adjustmentValue) {
-            $('.bg').css('background-color', 'black');
-        } else if( contactSection - adjustmentValue >= currentPosition && currentPosition >= portfolioSection - adjustmentValue) {
-            $('.bg').css('background-color', '#ccc');
-        } else {
-            $('.bg').css('background-color', 'black');
-        };
-    });
+		//背景色変更用
+		let aboutSection = $('#about').offset().top;
+		let skillsSection = $('#skills').offset().top;
+		let portfolioSection = $('#portfolio').offset().top;
+		let contactSection = $('#contact').offset().top;
+
+		let screenHeight = $(window).height();
+		let adjustmentValue = screenHeight / 2;　//調整値
+
+		if( skillsSection - adjustmentValue >= currentPosition && currentPosition >= aboutSection - adjustmentValue ) {
+			$('.bg').css('background-color', '#ccc');
+		} else if( portfolioSection - adjustmentValue >= currentPosition && currentPosition >= skillsSection - adjustmentValue) {
+			$('.bg').css('background-color', 'black');
+		} else if( contactSection - adjustmentValue >= currentPosition && currentPosition >= portfolioSection - adjustmentValue) {
+			$('.bg').css('background-color', '#ccc');
+		} else {
+			$('.bg').css('background-color', 'black');
+		};
+	});
 });
